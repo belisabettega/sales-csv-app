efa4f19c928cef12bf2753d02050bc2b324aac62
belisa bettega <belisabettega@gmail.com>
2021-08-31 19:30:52 -0300

    feat: method to store the data from the csv in the db

--- app/controllers/data_files_controller.rb
+++ app/controllers/data_files_controller.rb
@@ -5,15 +5,20 @@ class DataFilesController < ApplicationController
     @data_file = DataFile.new
   end
   def create
-    import(params[:data_file][:file])
+    data_file = DataFile.create
+    import(params[:data_file][:file], data_file)
     redirect_to root_path
   end
  
   private
  
-  def import(file)
+  def import(file, instance_of_file_obj)
     CSV.foreach(file, headers: :first_row, :col_sep => "\t") do |row|
-      puts row
+      merchant = Merchant.create(name: row["merchant name"], address: row["merchant address"])
+      purchaser = Purchaser.create(name: row["purchaser name"])
+      product = Product.create(description: row["item description"], price: row["item price"])
+      product_quantity = row["purchase count"]
+      Purchase.create(merchant: merchant, purchaser: purchaser, data_file: instance_of_file_obj, product_quantity: product_quantity, product: product)
     end
   end
 end
